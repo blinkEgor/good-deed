24e9a61e8b6937afa239941445b614b9fbf96802
blinkEgor <barminov44@gmail.com>
2023-11-22 15:14:43 +0300

    add translate for dashboards compounents

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -7,13 +7,13 @@ import {
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'Дом', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
+    name: 'Счета-фактуры',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Клиенты', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
