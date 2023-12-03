a21e1b6a528907810fbe14728e15a423a00bf529
blinkEgor <barminov44@gmail.com>
2023-12-03 20:10:36 +0300

    add delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -50,4 +50,9 @@ export async function updateInvoice(id: string, formData: FormData) {
    
     revalidatePath('/dashboard/goodDeed');
     redirect('/dashboard/goodDeed');
-  }
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/goodDeed');
+}
