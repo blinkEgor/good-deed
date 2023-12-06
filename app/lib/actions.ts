b045d3d991e7a6e3d47973f9ee1e6e3be9af4dfb
blinkEgor <barminov44@gmail.com>
2023-12-06 16:36:45 +0300

    fix-1.1.0 Type error: Cannot find name 'State'

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,14 +5,15 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
     invalid_type_error: 'Please select a customer.',
   }),
   amount: z.coerce
-  .number()
-  .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
   status: z.enum(['pending', 'paid'], {
     invalid_type_error: 'Please select an invoice status.',
   }),
@@ -22,7 +23,16 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice({}, formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -57,7 +67,7 @@ export async function createInvoice({}, formData: FormData) {
 
 export async function updateInvoice(
   id: string,
-  {},
+  prevState: State,
   formData: FormData,
 ) {
   const validatedFields = UpdateInvoice.safeParse({
