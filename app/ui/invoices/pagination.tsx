72c0c80d15373c0e7fb60cb61d22efab03656453
blinkEgor <barminov44@gmail.com>
2023-12-11 11:27:57 +0300

    change hover color

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -71,13 +71,13 @@ function PaginationNumber({
   isActive: boolean;
 }) {
   const className = clsx(
-    'flex h-10 w-10 items-center justify-center text-sm border',
+    'flex h-10 w-10 items-center justify-center text-sm border text-gray-100',
     {
       'rounded-l-md': position === 'first' || position === 'single',
       'rounded-r-md': position === 'last' || position === 'single',
-      'z-10 bg-blue-600 border-blue-600 text-white': isActive,
-      'hover:bg-gray-100': !isActive && position !== 'middle',
-      'text-gray-300': position === 'middle',
+      'z-10 bg-gray-600 border-gray-200 text-white': isActive,
+      'hover:bg-gray-500': !isActive && position !== 'middle',
+      'text-gray-100': position === 'middle',
     },
   );
 
@@ -100,10 +100,10 @@ function PaginationArrow({
   isDisabled?: boolean;
 }) {
   const className = clsx(
-    'flex h-10 w-10 items-center justify-center rounded-md border',
+    'flex h-10 w-10 items-center justify-center rounded-md border text-gray-100',
     {
       'pointer-events-none text-gray-300': isDisabled,
-      'hover:bg-gray-100': !isDisabled,
+      'hover:bg-gray-500': !isDisabled,
       'mr-2 md:mr-4': direction === 'left',
       'ml-2 md:ml-4': direction === 'right',
     },
