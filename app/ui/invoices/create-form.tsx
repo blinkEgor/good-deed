cf5b2c72652bd5e672c0b22d92431e30d74b6d7b
blinkEgor <barminov44@gmail.com>
2023-12-12 13:01:12 +0300

    change hover inside good deed create batton

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -133,7 +133,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/goodDeed"
-          className="flex h-10 items-center rounded-lg bg-gray-500 px-4 text-sm font-medium text-gray-200 transition-colors hover:bg-gray-200"
+          className="flex h-10 items-center rounded-lg bg-gray-500 px-4 text-sm font-medium text-gray-50 transition-colors hover:bg-gray-400"
         >
           Cancel
         </Link>
