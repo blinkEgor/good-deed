bf944aa53f53a9f1ed5d82ef6367d1c4d39e6441
blinkEgor <barminov44@gmail.com>
2023-12-14 15:15:14 +0300

    translate site to english

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ru">
+    <html lang="en">
       <body className={`${inter.className} bg-gray-800`}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -15,18 +15,18 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-700 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-white md:text-3xl md:leading-normal`}>
-            <strong>Добро пожаловать в приложение Good Deed.</strong> Тут ты можешь написать свой список добрых дел.
+            <strong>Welcome to Good Deed application.</strong> Here you can write your list of good deeds.
           </p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Авторизироваться</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Login</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <p className='w-64 text-center bg-gray-600 p-10 text-white rounded-lg'>
-            Тут будут снимки экрана приложения на разных устройстрвах
+            There will be screenshots of the application on different devices
           </p>
           {/* <Image 
             src='/hero-desktop.png' 
