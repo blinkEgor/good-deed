022a8a46f25ea087548636905ae0173b0867130e
blinkEgor <barminov44@gmail.com>
2023-12-15 10:24:15 +0300

    remove overview

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -20,7 +20,7 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl text-gray-200`}>
         Dashboard
       </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
@@ -32,7 +32,7 @@ export default async function Page() {
         <Suspense fallback={<LatestGoodDeedsSkeleton />}>
           <LatestInvoices />
         </Suspense>
-      </div>
+      </div> */}
     </main>
   );
 }
