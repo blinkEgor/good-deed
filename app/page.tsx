0b8e37cba97d056666786c18cfad368eefb57647
blinkEgor <barminov44@gmail.com>
2023-12-20 15:16:58 +0300

    integrate registration form

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,20 @@ export default function Page() {
           <p className={`${lusitana.className} text-xl text-white md:text-3xl md:leading-normal`}>
             <strong>Welcome to Good Deed application.</strong> Here you can write your list of good deeds.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Login</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          <div className='flex gap-5'>
+            <Link
+              href="/login"
+              className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Login</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+            <Link
+              href="/register"
+              className="flex items-center gap-5 self-start rounded-lg bg-green-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Registration</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+          </div>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <p className='w-64 text-center bg-gray-600 p-10 text-white rounded-lg'>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,15 @@
 import { Metadata } from 'next';
+// import RegistrationForm from '@/app/ui/registration-form';
  
 export const metadata: Metadata = {
   title: 'Register',
+};
+
+export default function RegistrationPage() {
+  return (
+    <div>
+      <h1>Registration form</h1>
+      {/* <RegistrationForm /> */}
+    </div>
+  );
 };
