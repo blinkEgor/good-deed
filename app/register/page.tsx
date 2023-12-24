5e03868a8ee030809d24932b677c8cdf7af28420
blinkEgor <barminov44@gmail.com>
2023-12-24 11:39:12 +0300

    registration prototype

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,15 +1,61 @@
-import { Metadata } from 'next';
-// import RegistrationForm from '@/app/ui/registration-form';
- 
-export const metadata: Metadata = {
-  title: 'Register',
-};
+'use client'
 
-export default function RegistrationPage() {
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface FormData {
+  username: string;
+  password: string;
+}
+
+export default function Register() {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const [registrationMessage, setRegistrationMessage] = useState('');
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    // Here, you would typically make an API request to your backend
+    // to handle user registration and store the data securely.
+
+    // For simplicity, we'll just log the details to the console.
+    console.log('Registering user:', data);
+
+    // Update the state or redirect the user after successful registration.
+    setRegistrationMessage('Registration successful!');
+  };
+
   return (
-    <div>
-      <h1>Registration form</h1>
-      {/* <RegistrationForm /> */}
+    <div className='text-gray-100'>
+      <h1>Registration Page</h1>
+      <form
+        className='flex-col items-center' 
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label>
+          Username:
+          <input
+            type="text"
+            {...register('username', { required: 'Username is required' })}
+          />
+        </label>
+        <div>{errors.username && <span>{errors.username.message}</span>}</div>
+
+        <br />
+
+        <label>
+          Password:
+          <input
+            type="password"
+            {...register('password', { required: 'Password is required' })}
+          />
+        </label>
+        <div>{errors.password && <span>{errors.password.message}</span>}</div>
+
+        <br />
+
+        <button type="submit">Register</button>
+      </form>
+
+      <div>{registrationMessage}</div>
     </div>
   );
 };
