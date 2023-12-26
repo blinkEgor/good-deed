e3bc66ca3545773790fb1874880576a0a536f9f5
blinkEgor <barminov44@gmail.com>
2023-12-26 17:10:00 +0300

    add register api

---
+++ app/api/register.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if(req.method === 'POST') {
+        const { username, password } = req.body;
+
+        // Call your server-side registration endpoint
+        const registrationResponse = await fetch('http://localhost:3000/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        if(registrationResponse.ok) {
+            const data = await registrationResponse.json();
+            res.status(201).json(data);
+        } else {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } else {
+        res.status(405).json({ error: 'Method Not Allowed' });
+    }
+}
