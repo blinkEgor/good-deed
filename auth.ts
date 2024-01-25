2e7cf44d64bfe637d0c4e5e895946205c1d40280
blinkEgor <barminov44@gmail.com>
2024-01-25 15:19:42 +0300

    work with session

--- auth.ts
+++ auth.ts
@@ -28,9 +28,10 @@ export const { auth, signIn, signOut } = NextAuth({
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
+
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
- 
+
           if (passwordsMatch) return user;
         }
         
@@ -39,4 +40,10 @@ export const { auth, signIn, signOut } = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      
+      return session;
+    }
+  }
 });
