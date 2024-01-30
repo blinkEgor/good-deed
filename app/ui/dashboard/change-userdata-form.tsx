ff38adb1531f0f06c13e40801f88c16a49f1ccab
blinkEgor <barminov44@gmail.com>
2024-01-30 09:25:53 +0300

    add email to form

--- app/ui/dashboard/change-userdata-form.tsx
+++ app/ui/dashboard/change-userdata-form.tsx
@@ -24,7 +24,7 @@ export default function ChangeUserdataForm() {
         <form action={formAction} className="space-y-3">
             <div className="flex-1 rounded-lg bg-gray-600 text-gray-100 px-6 pb-4 pt-8">
                 <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-                    Please registration to application.
+                    Please change your data to application.
                 </h1>
                 <div className="w-full">
                     {/* Username */}
@@ -42,6 +42,26 @@ export default function ChangeUserdataForm() {
                                 type="name"
                                 name="username"
                                 placeholder="Enter your username"
+                                required
+                            />
+                            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-200 peer-focus:text-gray-900" />
+                        </div>
+                    </div>
+                    {/* Email */}
+                    <div>
+                        <label
+                            className="mb-3 mt-5 block text-xs font-medium text-gray-50"
+                            htmlFor="email"
+                        >
+                            Email
+                        </label>
+                        <div className="relative">
+                            <input
+                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-200 bg-gray-600"
+                                id="email"
+                                type="email"
+                                name="email"
+                                placeholder="Enter your email"
                                 required
                             />
                             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-200 peer-focus:text-gray-900" />
